03a47dd7563feaae086d942e6db61c5bf1c71087
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2022-10-05 02:03:57 +0600

    total price adding

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -10,14 +10,20 @@ const Cart = ({cart}) => {
         shipping = shipping +product.shipping;
     }
 
+    let textt = (total * 10/100).toFixed(2);
+    let text = parseInt(textt);
 
+    let allTotal = total+shipping+text;
 
     return (
         <div>
             <p>oder summary</p>
             <p>value: {cart.length}</p>
             <p>price: {total}</p>
-            <p>{shipping}</p>
+            <p>shipping: {shipping}</p>
+            <p>Text: {text}</p>
+            <h3>all total: {allTotal}</h3>
+            {/* 49-4 Setting the cart data on the localStorage for future retrieve */}
         </div>
     );
 };
